f91b1b60c2722fbba8a7e75b321c7a6d9dc0cab3
cherry-mac <cherry@e4net.net>
2024-01-05 13:35:24 +0900

    Initial Commit

---
+++ E4Pay/E4PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  E4PayApp.swift
+//  E4Pay
+//
+//  Created by 20-N311 on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct E4PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
